46b9f72baa66abce71db62bb8b3adb02ac42f611
Siddharth Bhatia <siddharth.bhatia@wadhwaniai.org>
2019-06-27 04:45:22 +0000

    ran post training quantization of vgg19 on CIFAR10

--- net/quantization.py
+++ net/quantization.py
@@ -4,22 +4,32 @@ from sklearn.cluster import KMeans
 from scipy.sparse import csc_matrix, csr_matrix
 
 
-def apply_weight_sharing(model, bits=5):
+def apply_weight_sharing(model, bits=8, dev = "cuda"):
     """
     Applies weight sharing to the given model
     """
-    for module in model.children():
-        dev = module.weight.device
-        weight = module.weight.data.cpu().numpy()
-        shape = weight.shape
-        mat = csr_matrix(weight) if shape[0] < shape[1] else csc_matrix(weight)
-        min_ = min(mat.data)
-        max_ = max(mat.data)
+    # for module in model.children():
+    for name,param in model.named_parameters():
+        print(name, param.data.shape, param.data.numel())
+
+        shape = param.data.shape
+        weight = param.data.cpu().numpy()
+
+        min_ = np.min(weight)
+        max_ = np.max(weight)
         space = np.linspace(min_, max_, num=2**bits)
         kmeans = KMeans(n_clusters=len(space), init=space.reshape(-1,1), n_init=1, precompute_distances=True, algorithm="full")
-        kmeans.fit(mat.data.reshape(-1,1))
+        kmeans.fit(weight.reshape(-1,1))
+        # print("KMeans end")
+
+        # print("kmeans.labels_", kmeans.labels_)
+        # print("kmeans.cluster_centers_", kmeans.cluster_centers_)
+
         new_weight = kmeans.cluster_centers_[kmeans.labels_].reshape(-1)
-        mat.data = new_weight
-        module.weight.data = torch.from_numpy(mat.toarray()).to(dev)
+        # print("new_weight", new_weight)
+
+        # print("param.data[:5]", param.data[:5])
+        param.data = torch.from_numpy(new_weight).to(dev).resize_(shape)
+        # print("param.data[:5]", param.data[:5])
 
 
--- weight_share.py
+++ weight_share.py
@@ -1,30 +1,53 @@
 import argparse
 import os
-
 import torch
-
-from net.models import LeNet
+import vgg
 from net.quantization import apply_weight_sharing
 import util
 
 parser = argparse.ArgumentParser(description='This program quantizes weight by using weight sharing')
-parser.add_argument('model', type=str, help='path to saved pruned model')
+parser.add_argument('--model', type=str, required=False, help='path to saved pruned model')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='disables CUDA training')
 parser.add_argument('--output', default='saves/model_after_weight_sharing.ptmodel', type=str,
                     help='path to model output')
+parser.add_argument('--state_dict', default='saves/model_best_state_dict.pth', type=str,
+                    help='path to saved_dict')
 args = parser.parse_args()
 
 use_cuda = not args.no_cuda and torch.cuda.is_available()
 
+# model_orig = vgg.vgg19()
+# model_orig.features = torch.nn.DataParallel(model_orig.features)
+# model_orig = model_orig.cuda()
+# model_orig.load_state_dict(torch.load(args.state_dict))
+# model_orig.features = model_orig.features.module
 
+model = vgg.vgg19()
+model.features = torch.nn.DataParallel(model.features)
+model = model.cuda()
+model.load_state_dict(torch.load(args.state_dict))
+model.features = model.features.module
+
+
+print(model.state_dict().keys())
+
+for name,param in model.named_parameters():
+    print(name, param.data.shape, param.data.numel())
+
+# for name,param in model.named_parameters():
+#     print(name, param.data.shape, param.data.numel())
+#     print(param.data)
+#     break
+
+# model = model.module
 # Define the model
-model = torch.load(args.model)
+# model = torch.load(args.model)
 print('accuracy before weight sharing')
 util.test(model, use_cuda)
 
 # Weight sharing
-apply_weight_sharing(model)
+apply_weight_sharing(model, bits = 1)
 print('accuacy after weight sharing')
 util.test(model, use_cuda)
 
