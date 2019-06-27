45063c00e01203ca6f052441e99fcf05cb3b5e5d
Siddharth Bhatia <siddharth.bhatia@wadhwaniai.org>
2019-06-27 11:02:17 +0000

    made flattened vgg reproduce results of original

--- net/quantization.py
+++ net/quantization.py
@@ -2,6 +2,8 @@ import torch
 import numpy as np
 from sklearn.cluster import KMeans
 from scipy.sparse import csc_matrix, csr_matrix
+import flat
+from collections import OrderedDict
 
 
 def apply_weight_sharing(model, bits=8, dev = "cuda"):
@@ -31,5 +33,54 @@ def apply_weight_sharing(model, bits=8, dev = "cuda"):
         # print("param.data[:5]", param.data[:5])
         param.data = torch.from_numpy(new_weight).to(dev).resize_(shape)
         # print("param.data[:5]", param.data[:5])
+
+
+
+def flatten_model(model, bits=1, dev = "cuda"):
+
+    l = get_flattened_module_structure(model)
+    model_flat = flat.Flat(l)
+
+    return model_flat
+
+
+
+def get_flattened_module_structure(model, dev = "cuda"):
+
+    l = [(name.replace(".","-"),module) for name,module in model.named_modules() if (type(module) != torch.nn.Sequential and type(module) != type(model))]
+    return l
+
+
+def print_module_structure(model, dev = "cuda"):
+
+    l = [(name,module) for name,module in model.named_modules() if (type(module) != torch.nn.Sequential and type(module) != type(model))]
+    print("\nmodule structure\n")
+    print("len(l)", len(l))
+    for n,m in l:
+        print(n,type(m),m)
+
+
+def check_copy_model(model):
+
+    model_copy = type(model)()
+    model_copy.load_state_dict(model.state_dict())
+
+
+def get_formatted_state_dict(model):
+
+    sd = model.state_dict()
+    sd_formatted = OrderedDict()
+    for key in sd.keys():
+        sd_formatted[key.replace(".","-",1)] = sd[key]
+
+
+    return sd_formatted
+
+
+
+
+
+
+
 
 
--- weight_share.py
+++ weight_share.py
@@ -2,8 +2,10 @@ import argparse
 import os
 import torch
 import vgg
-from net.quantization import apply_weight_sharing
+# from net.quantization import apply_weight_sharing, check_module_structure
+from net.quantization import *
 import util
+from torchsummary import summary
 
 parser = argparse.ArgumentParser(description='This program quantizes weight by using weight sharing')
 parser.add_argument('--model', type=str, required=False, help='path to saved pruned model')
@@ -23,34 +25,29 @@ use_cuda = not args.no_cuda and torch.cuda.is_available()
 # model_orig.load_state_dict(torch.load(args.state_dict))
 # model_orig.features = model_orig.features.module
 
-model = vgg.vgg19()
+model = vgg.vgg19().cuda()
 model.features = torch.nn.DataParallel(model.features)
 model = model.cuda()
 model.load_state_dict(torch.load(args.state_dict))
 model.features = model.features.module
 
 
-print(model.state_dict().keys())
+model_flat = flatten_model(model)
+sd_formatted = get_formatted_state_dict(model)
+model_flat.load_state_dict(sd_formatted)
 
-for name,param in model.named_parameters():
-    print(name, param.data.shape, param.data.numel())
-
-# for name,param in model.named_parameters():
-#     print(name, param.data.shape, param.data.numel())
-#     print(param.data)
-#     break
 
 # model = model.module
 # Define the model
 # model = torch.load(args.model)
 print('accuracy before weight sharing')
 util.test(model, use_cuda)
+util.test(model_flat, use_cuda)
+# # Weight sharing
+# apply_weight_sharing(model, bits = 1)
+# print('accuacy after weight sharing')
+# util.test(model, use_cuda)
 
-# Weight sharing
-apply_weight_sharing(model, bits = 1)
-print('accuacy after weight sharing')
-util.test(model, use_cuda)
-
-# Save the new model
-os.makedirs('saves', exist_ok=True)
-torch.save(model, args.output)
+# # Save the new model
+# os.makedirs('saves', exist_ok=True)
+# torch.save(model, args.output)
